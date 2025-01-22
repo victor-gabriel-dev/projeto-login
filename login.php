66aeee099bb2591ca779724d1711a7d026810417
victor-gabriel-dev <victorg100vg@gmail.com>
2025-01-22 17:54:14 -0300

    Update login.php

--- login.php
+++ login.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>apredendo php</title>
 </head>
 <body>
-    
+    <?php // begin with a PHP open tag.
+
+    echo "Hello World\n";
+    print("Hello cheatsheets.zip");
+
+    ?>
 </body>
 </html>
